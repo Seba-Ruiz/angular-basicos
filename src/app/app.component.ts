eecd10d70f6ac01e4f84741a0fe045ac08f290c6
Seba Ruiz <sebarata@hotmail.com>
2021-08-17 17:22:11 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //nombre del componente
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  // titulo: string = 'Contador App';
+  // numero: number = 10;
+  // base: number = 5;
+
+
+  // sumar() {
+  //   this.numero +=1;
+  // }
+  // restar() {
+  //   this.numero -=1;
+  // }
+
+  // acumular( valor: number ) {
+  //   this.numero +=valor;
+  // }
+
 }
